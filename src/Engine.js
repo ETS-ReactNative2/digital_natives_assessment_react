5f08a83b47de79b242a2b22c7b449a21c6c85649
rizsoo <89772889+rizsoo@users.noreply.github.com>
2022-02-26 23:57:29 +0100

    it is working automaticly

--- src/Engine.js
+++ src/Engine.js
@@ -1,83 +1,66 @@
 function engine(n) {
 
-    let numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    let tens = [ '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
+    const numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tenners = [ '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
+    const theBigGuns = [ '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion',
+    'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion'];
 
     
     let input = String(n)
-    let inputLength = input.length;
     let inputSplit = input.split("");
 
-  //Create TENNERS
-    let singular = inputSplit.slice(inputLength -1);
-    let doublefigures = inputSplit.slice(inputLength -2, -1);
-    let doubles =  doublefigures + singular;
+    let valami = Number(input)
+    console.log(valami);
 
-    let twoFigures = function(n) {
-        let arr = n.length === 2 ? n.split("") : n;
-        let tenner = arr[0] > 0 ? tens[arr[0]] : "";
-        let single = arr[1] > 0 ? numbers[arr[1]] : "";
-        let hasDoubleFigures = arr[0] > 0 && arr[1] > 0 ? "-" : "";
-
-        if(n > 19) {
-            return tenner + hasDoubleFigures + single;
-        } else return numbers[Math.round(n *1)];
-    }
-
-  //Create HUNDREDS
-    let hundreds = inputSplit.slice(inputLength -3, -2);
-    let threeFigures = function(n) {
-        if(n > 0) { return numbers[n] + " hundred " 
-        } else return ""
-    } 
-
-  //Create THOUSANDS
-    let thousands = inputLength < 5 ? inputSplit.slice(inputLength -4, -3).join("") : inputSplit.slice(inputLength -5, -3).join("");
-    let fourFigures = function(n) {
-        console.log(n);
-        let isZero = n == 0 ? "thousand " : " thousand ";
-        if(n > 0 || inputLength > 3) { return twoFigures(n) + isZero;
-        } else return ""
-    } 
-  //Houndred Thousands...
-    let hundredThousands = inputSplit.slice(inputLength -6 , -5);
-  //   let nineFigures = function(n) {
-  //     if(n > 0 || inputLength > 8) { return numbers[n] + " hundred " 
-  //     } else return ""
-  // } 
-
-  //Millions :)
-    let millions = inputSplit.slice(inputLength -7 && -8, -6).join("");
-    let thoseMillions = function(n) {
-        if(n > 0 || inputLength > 8) { return twoFigures(n) + " million " 
-        } else return ""
-    } 
-    //Hundred Millions!
-    let hundredMillions = inputSplit.slice(inputLength -9, -8);
-
-  //And(s)?
-    let earlyGame = Math.round(doubles * 1) > 0 && inputLength > 3 ? "and " : "";
-    let lessEarlyGame = inputLength > 5 && thousands > 0 ? "and " : "";
-    let theGame = inputLength > 8 && millions > 0 ? "and " : "";
     
   //RESULT
     let result = ""
-    if(input > 0 && inputLength < 10) {
-        result = 
-        threeFigures(hundredMillions) +
-        theGame +
-        thoseMillions(millions) +
-        threeFigures(hundredThousands) + 
-        lessEarlyGame +
-        fourFigures(thousands) + 
-        threeFigures(hundreds) + 
-        earlyGame + 
-        twoFigures(doubles);
+    if(input > 0) {
+        let mapedInput = inputSplit.map(n => n);
+        let reversedMapedInput = mapedInput.reverse().join("");
+        let devidedEachThird = reversedMapedInput.match(/.{1,3}/g);
+
+        let threeFigures = (n) => {
+          let arr = String(n).split("").reverse();
+          console.log(arr);
+          
+          let ones = arr.slice(-1);
+          let tens = arr.slice(-2, -1);
+          let huns = arr.slice(-3, -2);
+          
+          let isTenner = [tens, ones];
+          
+          //AND
+          let and = Number(input) > 99 && isTenner.join("") > 0 && arr.length === 3 ? "and " : "";
+          
+          let hunner = huns > 0 ? numbers[huns] + ' hundred ' : "";
+          let twinner = ones > 0 && tens > 0 ? tenners[tens] + '-' : tenners[tens];
+          let single = ones === 0 ? '' : numbers[ones];
+
+          console.log(huns);
+
+          if(isTenner.join("") < 20) {
+                twinner = "";
+                single = numbers[Math.round(isTenner.join("") *1)];
+              } 
+          
+          return hunner + and + twinner + single
+
+          }
+
+          console.log(devidedEachThird);
+
+
+        //From 100 to the sky
+        if(n < 1000) {
+          result = threeFigures(devidedEachThird[0]).trim();
+        } else {
+          result = devidedEachThird.map((num, i) => num > 0 ? `${threeFigures(num).trim()} ${theBigGuns[i]} ` : "").reverse().join("").trim();
+        }
+
     } else if (input === "0") {
         result = "zero"
-    } else if (input.length > 9) {
-        result = "not possible"
-    }
+    } 
     return result;
 }
 
--- src/Engine.test.js
+++ src/Engine.test.js
@@ -3,9 +3,12 @@ const engine = require('./Engine');
 test('get test results', () => {
   expect(engine(7)).toBe("seven");
   expect(engine(42)).toBe("forty-two");
+  expect(engine(100)).toBe("one hundred");
+  expect(engine(101)).toBe("one hundred and one");
   expect(engine(1999)).toBe("one thousand nine hundred and ninety-nine");
   expect(engine(2001)).toBe("two thousand and one");
   expect(engine(17999)).toBe("seventeen thousand nine hundred and ninety-nine");
   expect(engine(342251)).toBe("three hundred and forty-two thousand two hundred and fifty-one");
   expect(engine(1300420)).toBe("one million three hundred thousand four hundred and twenty");
+  expect(engine(100100020)).toBe("one hundred million one hundred thousand and twenty");
 });
